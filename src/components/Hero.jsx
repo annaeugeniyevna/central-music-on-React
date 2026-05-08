493347d3d8242d526a06faf53c554fe03c92b188
Anna <annapaschenko828@gmail.com>
2026-05-08 19:05:56 +0300

    feat: add responsive style for Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,12 +11,14 @@ const Hero = () => {
     return (
         <section 
         style={bgStyle}
-        className="w-full h-[510px] pt-50 relative">
+        className="w-full h-[510px] pt-50 relative
+        md:h-[580px]">
             <div className="absolute inset-0 bg-black/40 z-0"></div>
             <div
             className="max-w-[1440px] mx-auto px-[5%] relative px-10">
                 <h1 
-                className="max-w-[280px] font-montserrat font-bold text-4xl text-white leading-tight text-center mx-auto">
+                className="max-w-[280px] font-montserrat font-bold text-4xl text-white leading-tight text-center mx-auto
+                md:text-5xl md:max-w-[380px] md:text-left md:mx-0">
                     Fullfilling dreams with strings attached
                 </h1>
             </div>
