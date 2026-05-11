ba4bc9e71da63355b3dca58a34af55161b911ef2
Anna <annapaschenko828@gmail.com>
2026-05-11 12:22:14 +0300

    feat: add responsive style for PromoBanner section

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -11,22 +11,25 @@ const PromoBanner = () => {
         <>
             <section
             style={bgPromo} 
-            className="relative z-20 max-w-[320px] min-h-[200px] rounded-xl py-4 mx-auto -mt-24">
+            className="relative z-20 max-w-[320px] min-h-[200px] rounded-xl py-4 mx-auto -mt-24
+            md:max-w-[680px] md:min-h-[210px] md:-mt-27 md:py-9">
                 <div
                 className="max-w-[1440px] mx-auto px-[5%]">
                     <div className="mb-5">
                         <h2
-                        className="font-work font-bold text-2xl text-white text-center pb-2">
+                        className="font-work font-bold text-2xl text-white text-center pb-2
+                        md:text-3xl">
                            FREE GUITAR LESSONS!
                         </h2>
                         <p
-                        className="font-work font-semibold text-xs text-white text-center">
+                        className="font-work font-semibold text-xs text-white text-center
+                        md:text-base md:max-w-[350px] md:mx-auto md:leading-relaxed">
                             Purchase any guitar over $499.99 and recieve a one-hour guitar lesson free. 
                         </p>
                     </div>
                     <div className="flex justify-center">
                         <a href="/"
-                        className="font-work font-semibold text-xs text-white bg-btn-bg py-3 px-4 rounded-full">
+                        className="font-work font-semibold text-xs text-white bg-btn-bg py-2 px-4 rounded-full">
                             LEARN MORE
                         </a>
                     </div>
