848f04346298c13c5e3766de2962b88112814373
Anna <annapaschenko828@gmail.com>
2026-05-11 17:54:13 +0300

    feat: add responsive style for PromoBanner section

--- src/components/PromoBanner.jsx
+++ src/components/PromoBanner.jsx
@@ -12,13 +12,15 @@ const PromoBanner = () => {
             <section
             style={bgPromo} 
             className="relative z-20 max-w-[320px] min-h-[200px] rounded-xl py-4 mx-auto -mt-24
-            md:max-w-[680px] md:min-h-[210px] md:-mt-27 md:py-9">
+            md:max-w-[680px] md:min-h-[210px] md:-mt-27 md:py-9
+            lg:max-w-[1100px]">
                 <div
                 className="max-w-[1440px] mx-auto px-[5%]">
                     <div className="mb-5">
                         <h2
                         className="font-work font-bold text-2xl text-white text-center pb-2
-                        md:text-3xl">
+                        md:text-3xl
+                        lg:text-4xl">
                            FREE GUITAR LESSONS!
                         </h2>
                         <p
@@ -29,7 +31,8 @@ const PromoBanner = () => {
                     </div>
                     <div className="flex justify-center">
                         <a href="/"
-                        className="font-work font-semibold text-xs text-white bg-btn-bg py-2 px-4 rounded-full">
+                        className="font-work font-semibold text-xs text-white bg-btn-bg py-2 px-4 rounded-full
+                        transition-all duration-300 hover:scale-105">
                             LEARN MORE
                         </a>
                     </div>
