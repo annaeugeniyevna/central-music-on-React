fb42496780f5c40f1a6923b3fe921ec4f58dfb26
Anna <annapaschenko828@gmail.com>
2026-05-11 18:47:45 +0300

    feat: add responsive style for Finds section

--- src/components/Finds.jsx
+++ src/components/Finds.jsx
@@ -3,6 +3,9 @@ import cableGoldish from "../assets/images/tweed-cable-goldish.png";
 import tunerPedal from "../assets/images/guitar-tuner-pedal.png";
 import Strings from "../assets/images/guitar-strings.png";
 
+// Variable for cards
+const cardStyle = "transition-all duration-300 hover:scale-105 cursor-pointer";
+
 const Finds = () => {
     return (
         <section 
@@ -19,13 +22,83 @@ const Finds = () => {
                 </h2>
                 <div
                 className="flex flex-col gap-9
-                md:grid md:grid-cols-2 md:gap-8">
+                md:grid md:grid-cols-2 md:gap-8
+                lg:flex lg:flex-row lg:justify-center">
                     <div 
-                    className="max-w-[324px] bg-white shadow-md rounded-lg pb-7
-                    md:mb-3">
+                    className={`max-w-[324px] bg-white shadow-md rounded-lg pb-7
+                    md:mb-3
+                    lg:min-w-[380px] lg:mb-0
+                    ${cardStyle}`}>
                         <img 
                         src={cableAqua} alt="Guitar gold"
-                        className="mx-auto"/>
+                        className="mx-auto 
+                        lg:min-w-[380px]"/>
+                        <div 
+                        className="flex justify-between mx-6 mt-4">
+                            <h3
+                            className="font-work font-bold text-lg text-dark-gray">
+                                Brand Name
+                            </h3>
+                            <p
+                            className="font-work font-bold text-lg text-dark-gray">
+                                $2,995
+                            </p>
+                        </div>
+                        <p
+                        className="font-work font-medium text-base text-medium-gray ml-6 mt-1 mb-6">
+                            Product description here
+                        </p>
+                        <a href="/"
+                        className="font-montserrat font-regular text-sm text-gray px-2 py-1 border border-smoke rounded-full ml-6">
+                            <span
+                            className="font-bold text-base mr-1">
+                                0 
+                            </span>
+                            colors available
+                        </a>
+                    </div>
+                    <div 
+                    className={`max-w-[324px] bg-white shadow-md rounded-lg pb-7
+                    md:mb-3
+                    lg:min-w-[380px] lg:mb-0
+                    ${cardStyle}`}>
+                    <img 
+                    src={cableGoldish} alt="Guitar gold"
+                    className="mx-auto
+                    lg:min-w-[380px]"/>
+                        <div 
+                        className="flex justify-between mx-6 mt-4">
+                            <h3
+                            className="font-work font-bold text-lg text-dark-gray">
+                                Brand Name
+                            </h3>
+                            <p
+                            className="font-work font-bold text-lg text-dark-gray">
+                                $2,995
+                            </p>
+                        </div>
+                        <p
+                        className="font-work font-medium text-base text-medium-gray ml-6 mt-1 mb-6">
+                            Product description here
+                        </p>
+                        <a href="/"
+                        className="font-montserrat font-regular text-sm text-gray px-2 py-1 border border-smoke rounded-full ml-6">
+                            <span
+                            className="font-bold text-base mr-1">
+                                0 
+                            </span>
+                            colors available
+                        </a>
+                    </div>
+                    <div 
+                    className={`max-w-[324px] bg-white shadow-md rounded-lg pb-7
+                    md:mb-3
+                    lg:min-w-[380px]
+                    ${cardStyle}`}>
+                    <img 
+                    src={tunerPedal} alt="Guitar gold"
+                    className="mx-auto
+                    lg:min-w-[380px]"/>
                         <div 
                         className="flex justify-between mx-6 mt-4">
                             <h3
@@ -52,65 +125,7 @@ const Finds = () => {
                     </div>
                     <div 
                     className="max-w-[324px] bg-white shadow-md rounded-lg pb-7
-                    md:mb-3">
-                    <img 
-                    src={cableGoldish} alt="Guitar gold"
-                    className="mx-auto"/>
-                        <div 
-                        className="flex justify-between mx-6 mt-4">
-                            <h3
-                            className="font-work font-bold text-lg text-dark-gray">
-                                Brand Name
-                            </h3>
-                            <p
-                            className="font-work font-bold text-lg text-dark-gray">
-                                $2,995
-                            </p>
-                        </div>
-                        <p
-                        className="font-work font-medium text-base text-medium-gray ml-6 mt-1 mb-6">
-                            Product description here
-                        </p>
-                        <a href="/"
-                        className="font-montserrat font-regular text-sm text-gray px-2 py-1 border border-smoke rounded-full ml-6">
-                            <span
-                            className="font-bold text-base mr-1">
-                                0 
-                            </span>
-                            colors available
-                        </a>
-                    </div>
-                    <div 
-                    className="max-w-[324px] bg-white shadow-md rounded-lg pb-7">
-                    <img 
-                    src={tunerPedal} alt="Guitar gold"
-                    className="mx-auto"/>
-                        <div 
-                        className="flex justify-between mx-6 mt-4">
-                            <h3
-                            className="font-work font-bold text-lg text-dark-gray">
-                                Brand Name
-                            </h3>
-                            <p
-                            className="font-work font-bold text-lg text-dark-gray">
-                                $2,995
-                            </p>
-                        </div>
-                        <p
-                        className="font-work font-medium text-base text-medium-gray ml-6 mt-1 mb-6">
-                            Product description here
-                        </p>
-                        <a href="/"
-                        className="font-montserrat font-regular text-sm text-gray px-2 py-1 border border-smoke rounded-full ml-6">
-                            <span
-                            className="font-bold text-base mr-1">
-                                0 
-                            </span>
-                            colors available
-                        </a>
-                    </div>
-                    <div 
-                    className="max-w-[324px] bg-white shadow-md rounded-lg pb-7">
+                    lg:hidden">
                     <img 
                     src={Strings} alt="Guitar gold"
                     className="mx-auto"/>
